20abb145e4614912e05b887741df6286f291bafe
Matheus <matheusdsouzabuth@gmail.com>
2025-08-20 09:43:03 -0300

    All project

---
+++ src/desafio_poo_dio/domain/Curso.java
@@ -0,0 +1,36 @@
+package desafio_poo_dio.domain;
+
+public class Curso extends Conteudo{
+
+    private int cargaHoraria;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    public Curso(String titulo, String descricao, int cargaHoraria) {
+		super(titulo, descricao);
+		this.cargaHoraria = cargaHoraria;
+	}
+
+	public Curso() {
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
